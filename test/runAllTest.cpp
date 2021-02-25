fd5cbe9fa00c1287e5252f18ac3f88744a7e2764
tommasobucaioni <tbucaioni@virgilio.it>
2021-02-25 17:23:04 +0100

    test started, first test on constructor

--- test/runAllTest.cpp
+++ test/runAllTest.cpp
@@ -2,3 +2,9 @@
 // Created by Tommaso Bucaioni on 19/02/21.
 //
 
+#include "/Users/tommasobucaioni/CLionProject/Laboratorio/test/lib/google_tests/googletest/include/gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
--- test/test.cpp
+++ test/test.cpp
@@ -2,3 +2,13 @@
 // Created by Tommaso Bucaioni on 19/02/21.
 //
 
+
+#include "/Users/tommasobucaioni/CLionProject/Laboratorio/IniFile.cpp"
+#include "/Users/tommasobucaioni/CLionProject/Laboratorio/IniFile.h"
+#include "/Users/tommasobucaioni/CLionProject/Laboratorio/test/lib/google_tests/googletest/include/gtest/gtest.h"
+
+//constructor tester
+TEST(IniFileTest, ConstructorTest) {
+    IniFile constructorTester("IniFile.ini");
+    ASSERT_EQ(constructorTester.getFileName(), "IniFile.ini");
+}
